952cf696b76aa57fb514a45adb149a9f8f3d5bb7
gersar08 <nagerman26@gmail.com>
2024-10-19 20:09:10 -0600

    Delete Useless files

--- src/utils/rolesPermissions.js
+++
@@ -1,7 +0,0 @@
-const rolesPermissions = {
-    admin: ["usuarios", "inventario", "facturacion", "registro-ventas"],
-    user: ["opción1", "opción2"],
-    guest: [],
-  };
-
-  export default rolesPermissions;
